d00cf9ea20d08ed8cc3b1c1cadd30ea3827c2548
OxidFrontEnd <vladimir.stportal@gmail.com>
2022-06-23 07:34:21 +0300

    create header slick-slider

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,16 @@ $(function(){
 
     $('.header__slider').slick({
         infinite: true,
-        fade: true
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-ride.svg" alt="">',
+        asNavFor: '.slider-dots',
     });
+
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider',
+    })
 
 });
